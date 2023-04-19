f5cfe732e0759cebcbc3008150f70eba64783e41
BeautifulBabe-MTM <35228898+zak10110@users.noreply.github.com>
2023-04-19 12:41:45 +0300

    Shoto pohozoe na pravdu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { XyzComponent } from './xyz/xyz.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { FruitsComponent } from './fruits/fruits.component';
-
+import{HttpClientModule} from '@angular/common/http';
 @NgModule({
   declarations: [
     
